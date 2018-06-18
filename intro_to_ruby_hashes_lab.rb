e4f61e8fef3400c507f3fe2f975aae020e63ab56
Yan Wang <wangyan6783@gmail.com>
2018-06-18 18:44:43 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,34 +29,13 @@ end
 def monopoly_with_fourth_tier
   monopoly = {railroads: {
     pieces: 4,
-    names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}},
+    names: {reading_railroad: {"mortgage_value" => "$100"}, pennsylvania_railroad: {"mortgage_value" => "$200"}, b_and_o_railroad: {"mortgage_value" => "$400"}, shortline: {"mortgage_value" => "$800"}},
     rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
   }}
 end
  
 
-  describe "fourth level of monopoly hash" do
-    describe "#monopoly_with_fourth_tier" do
-      it "sets the 1st key of the :reading_railroad hash to a string, 'mortgage_value', whose value is '$100'" do
 
-        expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:reading_railroad)).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[0].has_key?("mortgage_value")).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[0].values[0]).to eq("$100")
-      end
-
-      it "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'" do
-
-        expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:pennsylvania_railroad)).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[1].has_key?("mortgage_value")).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[1].values[0]).to eq("$200")
-      end
-
-      it "sets the 1st key of the :b_and_o_railroad hash to a string, 'mortgage_value', whose value is '$400'" do
-
-        expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:b_and_o_railroad)).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[2].has_key?("mortgage_value")).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[2].values[0]).to eq("$400")
-      end
 
       it "sets the 1st key of the :shortline hash to a string, `mortgage_value`, whose value is '$800'" do
 
