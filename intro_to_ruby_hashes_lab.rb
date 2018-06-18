289955caeac497200056544dcb5d817fc5817c27
Yan Wang <wangyan6783@gmail.com>
2018-06-18 18:44:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,19 +33,6 @@ def monopoly_with_fourth_tier
     rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
   }}
 end
- 
-
-
-
-      it "sets the 1st key of the :shortline hash to a string, `mortgage_value`, whose value is '$800'" do
-
-        expect(monopoly_with_fourth_tier.values[0][:names].keys[3]).to eq(:shortline)
-        expect(monopoly_with_fourth_tier.values[0][:names].values[3].has_key?("mortgage_value")).to be true
-        expect(monopoly_with_fourth_tier.values[0][:names].values[3].values[0]).to eq("$800")
-      end
-    end
-  end
-end
 
 
 
